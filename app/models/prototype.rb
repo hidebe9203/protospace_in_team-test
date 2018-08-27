dc6a66b3d760033ac60e6311594e12d624e2c53f
hidebe9203 <kskhidebe9203@gmail.com>
2018-08-27 13:54:10 +0900

    commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,25 @@
+class Prototype < ActiveRecord::Base
+  belongs_to :user
+  has_many :captured_images, dependent: :destroy
+  has_many :comments, :likes, :prototype_tags
+  has_many :tags, through: :prototype_tags
+
+  accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
+
+  validates :title,
+            :catch_copy,
+            :concept,
+            presence: true
+
+  def reject_sub_images(attributed)
+    attributed['content'].blank?
+  end
+
+  def set_main_thumbnail
+    captured_images.main.first.content
+  end
+
+  def posted_date
+    created_at.strftime('%b %d %a')
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, UserImageUploader
+
+  has_many :prototypes
+  has_many :likes 
+  has_many :comments
+
+  validates :name, presence: true
+end
